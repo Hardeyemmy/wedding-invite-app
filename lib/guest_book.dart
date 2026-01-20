acb05029dd73f23036655ae50aa7d9e2ffdabd83
Hardeyemmy <iiadeyemi307@gmail.com>
2026-01-20 04:14:12 +0100

    writing to firestore and reading the message on the homescreen

--- lib/guest_book.dart
+++ lib/guest_book.dart
@@ -1,11 +1,15 @@
 import 'dart:async';
+import 'guest_book_message.dart';
 import 'package:flutter/material.dart';
-import 'widgets.dart';
+import 'package:provider/provider.dart';
+import 'app_state.dart';
 
 class GuestBook extends StatefulWidget {
-  const GuestBook({required this.addMessages, super.key});
+  const GuestBook(
+      {required this.addMessages, required this.messages, super.key});
 
   final FutureOr<void> Function(String message) addMessages;
+  final List<GuestBookMessage> messages;
 
   @override
   State<GuestBook> createState() => _GuestBookState();
@@ -17,43 +21,61 @@ class _GuestBookState extends State<GuestBook> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Form(
-        key: _formkey,
-        child: Row(
-          children: [
-            Expanded(
-                child: TextFormField(
-              controller: _messageController,
-              decoration:
-                  const InputDecoration(hintText: 'Send us your wishes'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Kindly Enter your wishes';
-                }
-                return null;
-              },
-            )),
-            const SizedBox(width: 8.0),
-            ElevatedButton(
-              onPressed: () async {
-                if (_formkey.currentState!.validate()) {
-                  await widget.addMessages(_messageController.text);
-                  _messageController.clear();
-                }
-              },
-              child: const Row(
-                children: [
-                  Icon(Icons.send),
-                  SizedBox(width: 4.0),
-                  Text('Send Wishes')
-                ],
-              ),
-            )
-          ],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+            key: _formkey,
+            child: Row(
+              children: [
+                Expanded(
+                    child: TextFormField(
+                  controller: _messageController,
+                  decoration:
+                      const InputDecoration(hintText: 'Send us your wishes'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Kindly Enter your wishes';
+                    }
+                    return null;
+                  },
+                )),
+                const SizedBox(width: 8.0),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (_formkey.currentState!.validate()) {
+                      await widget.addMessages(_messageController.text);
+                      _messageController.clear();
+                    }
+                  },
+                  child: const Row(
+                    children: [
+                      Icon(Icons.send),
+                      SizedBox(width: 4.0),
+                      Text('Send Wishes')
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
-      ),
+        const SizedBox(height: 8.0),
+        Consumer<AppState>(
+          builder: (context, appState, _) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                for (final message in appState.guestBookMessages)
+                  Text('${message.name}: ${message.message}'),
+              ],
+            );
+          },
+        ),
+        const SizedBox(height: 8.0),
+      ],
     );
   }
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -21,7 +21,7 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('assets/pics.jpg', height: 200, width: 250),
+            Image.asset('assets/pics.jpg', height: 100, width: 250),
             const SizedBox(height: 8.0),
             const IconAndText(Icons.calendar_month_rounded, 'Jan 25'),
             const IconAndText(Icons.location_city_sharp, 'Lagos, Nigeria'),
@@ -42,6 +42,7 @@ class HomeScreen extends StatelessWidget {
                           GuestBook(
                             addMessages: (message) =>
                                 auth.addMessageToGuestBook(message),
+                            messages: auth.guestBookMessages,
                           )
                         ]
                       ],
